4e4b6b713e60036579a414c2cb1aef5e0a218cda
Jeff Evans <reznet@gmail.com>
2013-10-08 00:01:18 -0700

    update namespaces for classes now in S3M project

--- TestProject2/ParserTests.cs
+++ TestProject2/ParserTests.cs
@@ -3,7 +3,7 @@ using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using S3MParser;
+using S3M;
 
 namespace TestProject2
 {

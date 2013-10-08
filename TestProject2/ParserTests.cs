63144079b874a37acb28cc30d8bd3fc96b345b75
Jeff Evans <reznet@gmail.com>
2013-10-07 20:58:20 -0700

    got project building

--- TestProject2/ParserTests.cs
+++ TestProject2/ParserTests.cs
@@ -14,53 +14,6 @@ namespace TestProject2
     [DeploymentItem("01-v-drac.s3m")]
     public class ParserTests
     {
-        public ParserTests()
-        {
-            //
-            // TODO: Add constructor logic here
-            //
-        }
-
-        private TestContext testContextInstance;
-
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
-
-        #region Additional test attributes
-        //
-        // You can use the following additional attributes as you write your tests:
-        //
-        // Use ClassInitialize to run code before running the first test in the class
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // Use ClassCleanup to run code after all tests in a class have run
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Use TestInitialize to run code before running each test 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // Use TestCleanup to run code after each test has run
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
-        #endregion
-
         private readonly string vDracFilename = "01-v-drac.s3m";
 
         private S3MFile GetTestFile()

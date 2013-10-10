d2d046cc0595ae0051a449826946204233f174ab
Jeff Evans <reznet@gmail.com>
2013-10-09 23:45:03 -0700

    output tempo midi event

---
+++ S3mToMidi/TempoEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S3MParser
+{
+    class TempoEvent : Event
+    {
+        private readonly int _tempoBpm;
+
+        public TempoEvent(int tick, int tempoBpm)
+            : base(tick)
+        {
+            Debug.Assert(tempoBpm > 0, "tempo must be a positive integer: " + tempoBpm);
+            _tempoBpm = tempoBpm;
+        }
+
+        public int TempoBpm { get { return _tempoBpm; } }
+    }
+}

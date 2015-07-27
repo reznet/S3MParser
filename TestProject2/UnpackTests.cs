dcc1f0e544ada99392a6973618ed999c7bfde575
Jeff Evans <reznet@gmail.com>
2015-07-26 21:10:07 -0500

    unpack short correctly
    The bytes of a short were being unpacked backwards.  this fixes it and
    adds unit tests.  note that there aren't any 32bit ints being read, so
    those tests will fail for now until I find a use for the method and will
    then fix the tests.

---
+++ TestProject2/UnpackTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using S3M;
+
+namespace TestProject2
+{
+    [TestClass]
+    public class UnpackTests
+    {
+        [TestMethod]
+        public void ReadShortAsIntWithFirstByteSet()
+        {
+            ReadBytesAndAssertExpected(new[] { (byte)1, (byte)0 }, 1);
+        }
+
+        [TestMethod]
+        public void ReadShortAsIntWithSecondByteSet()
+        {
+            ReadBytesAndAssertExpected(new[] { (byte)0, (byte)1 }, 16);
+        }
+
+        [TestMethod]
+        public void ReadByteAsInt()
+        {
+            ReadBytesAndAssertExpected(new[] { (byte)1 }, 1);
+        }
+
+        [TestMethod]
+        public void ReadLongAsIntWithFirstByteSet()
+        {
+            ReadBytesAndAssertExpected(new[] { (byte)1, (byte)0, (byte)0, (byte)0 }, 1);
+        }
+
+        [TestMethod]
+        public void ReadLongAsIntWithSecondByteSet()
+        {
+            ReadBytesAndAssertExpected(new[] { (byte)0, (byte)1, (byte)0, (byte)0 }, 1);
+        }
+
+        [TestMethod]
+        public void ReadLongAsIntWithThirdByteSet()
+        {
+            ReadBytesAndAssertExpected(new[] { (byte)0, (byte)0, (byte)1, (byte)0 }, 1);
+        }
+
+        [TestMethod]
+        public void ReadLongAsIntWithFourthByteSet()
+        {
+            ReadBytesAndAssertExpected(new[] { (byte)0, (byte)0, (byte)0, (byte)1 }, 1);
+        }
+
+        private void ReadBytesAndAssertExpected(byte[] bytes, int expected)
+        {
+            using (var memoryStream = new MemoryStream(bytes))
+            {
+                using (BinaryReader reader = new BinaryReader(memoryStream))
+                {
+                    var actual = reader.ReadBytesAsInt(bytes.Length);
+                    Assert.AreEqual(expected, actual);
+                }
+            }
+        }
+    }
+}

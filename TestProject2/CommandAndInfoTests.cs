8fd92683cd00f845c9073c53fec2f295754b0e27
Jeff Evans <reznet@gmail.com>
2015-07-26 23:39:05 -0500

    rewriting how commands are parsed, part 1
    the code to parse commands is a mess.  this change tries to simplify by
    getting rid of the map which didn't handle all cases and will use a case
    statement instead.  unit tests will be written to verify the
    implementation too.  the map approach might have worked in a language
    like F# that supports currying, but because some commands have one
    parameter and others take two, the single map approach became too
    complicated.

---
+++ TestProject2/CommandAndInfoTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using S3M;
+using System.Globalization;
+
+namespace TestProject2
+{
+    [TestClass]
+    public class CommandAndInfoTests
+    {
+        [TestMethod]
+        public void Axx()
+        {
+            CreateAndVerify("A10", CommandType.SetSpeed, 16, 0);
+        }
+
+        [TestMethod]
+        public void Bxx()
+        {
+            CreateAndVerify("BAA", CommandType.JumpToOrder, 170, 0);
+        }
+
+        private static void CreateAndVerify(string commandAndInfo, CommandType expectedCommand, int expectedX, int expectedY)
+        {
+            var result = CreateCommandAndInfo(commandAndInfo);
+
+            Assert.AreEqual(expectedCommand, result.Commmand, "wrong command type");
+            Assert.AreEqual(expectedX, result.X, "wrong X");
+            Assert.AreEqual(expectedY, result.Y, "wrong Y");
+        }
+
+        private static CommandAndInfo CreateCommandAndInfo(string commandAndInfo)
+        {
+            return CommandAndInfo.Create(byte.Parse(commandAndInfo.Substring(0, 1), NumberStyles.HexNumber), byte.Parse(commandAndInfo.Substring(1), NumberStyles.HexNumber));
+        }
+    }
+}

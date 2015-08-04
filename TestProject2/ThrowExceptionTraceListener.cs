264f3ca8313c76c902f4c76c31d3075e6c97505e
Jeff Evans <reznet@gmail.com>
2015-08-04 00:00:38 -0500

    replace default trace listener
    This change replaces the default trace listener and disables displaying
    an assert dialog when running tests.  The replacement listener throws an
    exception when an assert fails.  This causes debug assert failures to
    cause the running test to fail, and prevents an assert dialog from
    blocking tests.  I don't know why this isn't the default for Visual
    Studio.

---
+++ TestProject2/ThrowExceptionTraceListener.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject2
+{
+    /// <summary>
+    /// A trace listener that throws an exception when a Debug.Assert or Debug.Fail fails.
+    /// </summary>
+    public class ThrowExceptionTraceListener : TraceListener
+    {
+        // add this trace listener to the app.config for an mstest-based test library
+        // and when a Debug.Assert fails the test will stop and the error will
+        // be logged as if the code had thrown an exception.
+        // 
+        //<system.diagnostics>
+        //    <trace>
+        //        <listeners>
+        //        <clear />
+        //        <add name="ThrowExceptionTraceListener" type="TestProject2.ThrowExceptionTraceListener, TestProject2" />
+        //        </listeners>
+        //    </trace>
+        //    <assert assertuienabled="false" />
+        //</system.diagnostics>
+
+        public override void Write(string message)
+        {
+            // do nothing.  the trace listener added by the test framework already writes messages to the test log
+        }
+
+        public override void WriteLine(string message)
+        {
+            // do nothing.  the trace listener added by the test framework already writes messages to the test log
+        }
+
+        public override void Fail(string message)
+        {
+            throw new AssertFailureException(message);
+        }
+
+        public override void Fail(string message, string detailMessage)
+        {
+            throw new AssertFailureException(message + Environment.NewLine + detailMessage);
+        }
+    }
+}

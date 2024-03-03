8153df9f511e727d54c8d06b9290ab03f30ce3ef
Jeff Evans <reznet@gmail.com>
2024-03-02 20:53:39 -0800

    convert Sm3ToMidi to dotnet

--- S3mToMidi/Program.cs
+++ S3mToMidi/Program.cs
@@ -1,23 +1,2 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.IO;
-using System.Reflection;
-using System.Xml;
-using System.Diagnostics;
-using S3M;
-
-namespace S3MParser
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string filename = args[0];
-            S3MFile file = S3MFile.Parse(filename);
-
-            MidiWriter2.Save(NoteEventGenerator.Generate(file).ToList(), Path.GetFileName(Path.ChangeExtension(filename, ".mid")));
-        }
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");

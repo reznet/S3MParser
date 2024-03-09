1201d9912926e819d0f5761653c2b05493967ddd
Jeff Evans <reznet@gmail.com>
2024-03-08 21:06:41 -0800

    improve time signature detection and add single pattern export support

--- S3mToMidi/NoteEventGeneratorOptions.cs
+++ S3mToMidi/NoteEventGeneratorOptions.cs
@@ -8,6 +8,15 @@ namespace S3MParser
 {
     public class NoteEventGeneratorOptions
     {
+        /// <summary>
+        /// Gets or sets a value indicating whether each midi channel is mapped to 
+        /// an instrument (the default) or is mapped to a pattern.
+        /// </summary>
         public bool ChannelsFromPatterns { get; set; }
+
+        /// <summary>
+        /// Gets or sets the single pattern to export.
+        /// </summary>
+        public int? Pattern { get; set; }
     }
 }

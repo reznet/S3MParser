354295bc095bb03e380e5838e90fed51168f7d5f
Jeff Evans <reznet@gmail.com>
2024-04-07 15:42:24 -0700

    skip drum channel 10 for non-drum parts

--- S3mToMidi/OutputChannel.cs
+++ S3mToMidi/OutputChannel.cs
@@ -9,6 +9,12 @@ namespace S3mToMidi
         public OutputChannel()
         {
             ChannelNumber = nextChannelNumber++;
+            if (ChannelNumber == 09) // 10 in zero-based counter is 09
+            {
+                // avoid General Midi channel 10 because players interpret it as a drum channel
+                Console.WriteLine("Skipping output drum channel 10");
+                ChannelNumber = nextChannelNumber++;
+            }
             Console.WriteLine("Creating output channel {0}", ChannelNumber);
             Debug.Assert(ChannelNumber <= 16, "output channel number out of range.");
         }
@@ -27,6 +33,7 @@ namespace S3mToMidi
 
         public void AddEvent(NoteEvent noteEvent)
         {
+            Debug.Assert(noteEvent.Channel == ChannelNumber, "trying to add a note event to the wrong output channel");
             AddEvent((Event)noteEvent);
             if (noteEvent.Type == NoteEvent.EventType.NoteOn)
             {

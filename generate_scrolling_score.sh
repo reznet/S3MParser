125187b6cf6db548a8e28beba32d8622ff09c874
Jeff Evans <reznet@gmail.com>
2024-05-05 19:30:12 -0700

    publish app before generating video

--- generate_scrolling_score.sh
+++ generate_scrolling_score.sh
@@ -4,7 +4,8 @@ path_to_me=${0:A:h}
 filename=${1}
 filename_no_extension=${filename:r}
 
-ECHO "$path_to_me/S3mToMidi/bin/Release/net8.0/publish/S3mToMidi"
+# make sure we're running with the latest code
+dotnet publish
 
 # generate lilypond score
 $path_to_me/S3mToMidi/bin/Release/net8.0/publish/S3mToMidi \

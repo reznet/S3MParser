ec57348684b73e5333c7d0df43d95568d19ec72b
Jeff Evans <reznet@gmail.com>
2024-05-13 22:42:32 -0700

    generate mp4 with rendered audio via ffmpeg

--- generate_scrolling_score.sh
+++ generate_scrolling_score.sh
@@ -10,12 +10,16 @@ dotnet publish
 # generate lilypond score
 $path_to_me/S3mToMidi/bin/Release/net8.0/publish/S3mToMidi \
     --file "${filename}" \
-    --exclude-channel 4 \
-    --exclude-channel 5 \
     --exporter lilypond
 
 # generate scrolling score
 ly2video -i "$filename_no_extension.ly" --padding 0,0
 
-# convert to mp4 for final cut pro
-ffmpeg -i "$filename_no_extension.avi" -c:v copy -c:a copy "$filename_no_extension.mp4"
+# generate audio file
+if [[ ! -a "$filename_no_extension.mp3" ]]; then
+    # ffmpeg downloaded from https://evermeet.cx/ffmpeg/ and compiled with --enable-libmodplug
+    /Users/jeff/Downloads/ffmpeg -y -i "$filename" "$filename_no_extension.mp3"
+fi
+
+# merge video and audio
+/Users/jeff/Downloads/ffmpeg -y -i "$filename_no_extension.avi" -i "$filename_no_extension.mp3" -c:v copy -map 0:v:0 -map 1:a:0 "$filename_no_extension.mp4"

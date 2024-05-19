e1c6b3eb5407da894b8c65f04520324e0b2494cb
Jeff Evans <reznet@gmail.com>
2024-05-18 22:32:34 -0700

    use schismtracker to generate audio

--- generate_scrolling_score.sh
+++ generate_scrolling_score.sh
@@ -5,6 +5,7 @@ set -e
 path_to_me=${0:A:h}
 filename=${1}
 filename_no_extension=${filename:r}
+schismtracker='/Volumes/Macintosh HD/Applications/Schism Tracker.app/Contents/MacOS/schismtracker'
 
 # make sure we're running with the latest code
 dotnet publish
@@ -18,10 +19,9 @@ $path_to_me/S3mToMidi/bin/Release/net8.0/publish/S3mToMidi \
 ly2video -i "$filename_no_extension.ly" --padding 0,0 --quality 1
 
 # generate audio file
-if [[ ! -a "$filename_no_extension.mp3" ]]; then
-    # ffmpeg downloaded from https://evermeet.cx/ffmpeg/ and compiled with --enable-libmodplug
-    /Users/jeff/Downloads/ffmpeg -y -i "$filename" "$filename_no_extension.mp3"
+if [[ ! -a "$filename_no_extension.wav" ]]; then
+    $schismtracker --play --diskwrite="$filename_no_extension.wav" "$filename"
 fi
 
 # merge video and audio
-/Users/jeff/Downloads/ffmpeg -y -i "$filename_no_extension.avi" -i "$filename_no_extension.mp3" -c:v copy -map 0:v:0 -map 1:a:0 "$filename_no_extension.mp4"
+/Users/jeff/Downloads/ffmpeg -y -i "$filename_no_extension.avi" -i "$filename_no_extension.wav" -c:v libx264 -crf 0 -pix_fmt yuv420p -map 0:v:0 -map 1:a:0 -c:a aac_at "$filename_no_extension.mp4"
